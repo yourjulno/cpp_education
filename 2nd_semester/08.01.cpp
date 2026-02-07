b3999c67487f04f11fa1736857cd9f7999ad47e5
yourjulno <diugaeva.iud@phystech.edu>
2026-02-07 12:56:35 +0300

    add another example of bad encapsulation

--- 2nd_semester/08.01.cpp
+++ 2nd_semester/08.01.cpp
@@ -1,7 +1,7 @@
 
 #include <cassert>
 
-namespace semester2 {
+namespace semester2::first {
 
 class Entity_v2
 {
@@ -29,11 +29,38 @@ void bad_reinterpret(Entity_v2 & entity_v2)
 
 }
 
+namespace semester2::first2 {
+
+#define private public
+
+class Entity_v2
+{
+private:
+    int a = 10;
+
+public:
+
+    int get_a() const
+    {
+        return a;
+    }
+};
+
+void bad_define(Entity_v2 & entity_v2)
+{
+    entity_v2.a = 13;
+}
+
+}
+
 int main()
 {
 
-    semester2::Entity_v2 entity_v2{};
-    semester2::bad_reinterpret(entity_v2);
-
+    semester2::first::Entity_v2 entity_v2{};
+    semester2::first::bad_reinterpret(entity_v2);
     assert(entity_v2.get_a() == 13);
+
+    semester2::first2::Entity_v2 entity_v2_define{};
+    semester2::first2::bad_define(entity_v2_define);
+    assert(entity_v2_define.get_a() == 13);
 }
